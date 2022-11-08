cbc098b1e891fa30df677c0ed6a49846944b94dc
Jeremy.Rio <rio.jeremy@outlook.fr>
2022-11-08 15:01:09 +0100

    Ferme v3, on peut ajouter un animal avec moins d'informations sur cet animal

--- Java/MangerManger.java
+++ Java/MangerManger.java
@@ -1,36 +1,32 @@
 import java.util.ArrayList;
 
-import javax.swing.plaf.synth.SynthScrollPaneUI;
-
 class Animaux{
-    String nom="";
-    String mange="";
-    String ferme="";
+    String nom="inconnu";
+    String mange="eau";
+    String ferme="maison";
+    String ordre="animal";
+    public Animaux(String mange){
+        this.mange=mange;
+    }
     public Animaux(String ferme, String nom, String mange){
         this.ferme=ferme;
         this.nom=nom;
         this.mange=mange;
     }
-    public void aTable(){
-        System.out.println(String.format("%s mange %s a la ferme %s", this.nom, this.mange, this.ferme));
-    }
-}
-class Classe extends Animaux{
-    String classif="";
-    public Classe(String ferme, String nom, String mange, String classif){
-        super(ferme, nom, mange);
-        this.classif=classif;
+    public Animaux(String ferme, String nom, String mange, String ordre){
+        this.ferme=ferme;
+        this.nom=nom;
+        this.mange=mange;
+        this.ordre=ordre;
     }
     public void aTable(){
-        System.out.println(String.format("%s qui est %s mange %s a la ferme %s", this.nom, this.classif, this.mange, this.ferme));
+        System.out.println(String.format("le %s %s se nourrit de %s à la %s", this.ordre, this.nom, this.mange, this.ferme));
     }
-
 }
 
 class Ferme{
     String nom="";
     ArrayList<Animaux> animals = new ArrayList<Animaux>();
-    ArrayList<Classe> classes = new ArrayList<Classe>();
     public Ferme(String nom){
         this.nom=nom;
     }
@@ -40,15 +36,9 @@ class Ferme{
     public void addAnimal(Animaux animal){
         animals.add(animal);
     }
-    public void addClasse(Classe classe){
-        classes.add(classe);
-    }
     public void afficher(){
         for (Animaux animal : animals){
             animal.aTable();
-        }
-        for (Classe classe : classes){
-            classe.aTable();
         }
     }
 }
@@ -57,7 +47,8 @@ public class MangerManger {
     public static void main(String[] args){
         Ferme f1= new Ferme("KFCfarm");
         f1.addAnimal(new Animaux("KFC","poule", "graines"));
-        f1.addAnimal(new Classe("Burgerking","vache", "herbe", "mammifere"));
+        f1.addAnimal(new Animaux("Burgerking","vache", "herbe", "mammifere"));
+        f1.addAnimal(new Animaux("cable electrique"));
         f1.afficher();
         // aTable() doit renvoyer poule:graines, vache:herbe, etc.
         // la vache est un mammifere, les animaux vivent dans une ferme
