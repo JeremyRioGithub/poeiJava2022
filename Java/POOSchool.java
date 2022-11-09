2cced81219f0943d5c484e602d0c47f6faa0661a
Jeremy.Rio <rio.jeremy@outlook.fr>
2022-11-09 12:03:03 +0100

    POOSchool v2.1 _ harmonized variable names and code format

--- Java/POOSchool.java
+++ Java/POOSchool.java
@@ -1,112 +1,131 @@
 import java.util.ArrayList;
 
-class Person{
-    String description="je fais partie de l'école";
-    String name="inconnu";
-    public Person(String namePerson){
-        this.name=namePerson;
+class Person {
+    String description = "je fais partie de l'école";
+    String name = "inconnu";
+
+    public Person(String namePerson) {
+        this.name = namePerson;
     }
-    public void whoAmI(){
+
+    public void whoAmI() {
         System.out.print(String.format("%s, %s", this.name, this.description));
     }
 }
 
-class Employee extends Person{
-    String desEmployee=", je suis employé";
-    //Integer salary=1000;
-    public Employee(String name){
+class Employee extends Person {
+    String description = ", je suis employé";
+
+    // Integer salary=1000;
+    public Employee(String name) {
         super(name);
     }
-    public void whoAmI(){
+
+    public void whoAmI() {
         super.whoAmI();
-        System.out.print(String.format("%s", this.desEmployee));
+        System.out.print(String.format("%s", this.description));
     }
 }
 
-class Principal extends Employee{
-    String desPrincipal=" et je suis responsable de l'etablissement et de ses employés";
-    public Principal(String name){
+class Principal extends Employee {
+    String description = " et je suis responsable de l'etablissement et de ses employés";
+
+    public Principal(String name) {
         super(name);
     }
-    public void whoAmI(){
+
+    public void whoAmI() {
         super.whoAmI();
-        System.out.print(String.format("%s", this.desPrincipal));
+        System.out.print(String.format("%s", this.description));
     }
 }
 
-class Teacher extends Employee{
-    String desTeacher=" et je donne des lecons";
-    String lesson="first lesson";
-    public Teacher(String name, String lesson){
+class Teacher extends Employee {
+    String description = " et je donne des lecons";
+    String lesson = "first lesson";
+
+    public Teacher(String name, String lesson) {
         super(name);
-        this.lesson=lesson;
+        this.lesson = lesson;
     }
-    public void whoAmI(){
+
+    public void whoAmI() {
         super.whoAmI();
-        System.out.print(String.format("%s", this.desTeacher));
+        System.out.print(String.format("%s", this.description));
     }
 }
 
-class Volunteer extends Person{
-    String desVolunteer=", je suis volontaire";
-    public Volunteer(String name){
+class Volunteer extends Person {
+    String description = ", je suis volontaire";
+
+    public Volunteer(String name) {
         super(name);
     }
-    public void whoAmI(){
+
+    public void whoAmI() {
         super.whoAmI();
-        System.out.print(String.format("%s", this.desVolunteer));
+        System.out.print(String.format("%s", this.description));
     }
 }
 
-class Kitchen extends Volunteer{
-    String desKitchen=" et je fait la cuisine";
-    public Kitchen(String name){
+class Kitchen extends Volunteer {
+    String description = " et je fait la cuisine";
+
+    public Kitchen(String name) {
         super(name);
     }
-    public void whoAmI(){
+
+    public void whoAmI() {
         super.whoAmI();
-        System.out.print(String.format("%s", this.desKitchen));
+        System.out.print(String.format("%s", this.description));
     }
 }
 
-class Cleaning extends Volunteer{
-    String desCleaning=" et je nettoie les salles";
-    public Cleaning(String name){
+class Cleaning extends Volunteer {
+    String description = " et je nettoie les salles";
+
+    public Cleaning(String name) {
         super(name);
     }
-    public void whoAmI(){
+
+    public void whoAmI() {
         super.whoAmI();
-        System.out.print(String.format("%s", this.desCleaning));
+        System.out.print(String.format("%s", this.description));
     }
 }
 
-class Student extends Person{
-    String desStudent=" et je vais à des cours à l'école";
-    String grade="";
-    public Student(String name,String grade){
+class Student extends Person {
+    String description = " et je vais à des cours à l'école";
+    String grade = "";
+
+    public Student(String name, String grade) {
         super(name);
-        this.grade=grade;
+        this.grade = grade;
     }
-    public void whoAmI(){
+
+    public void whoAmI() {
         super.whoAmI();
-        System.out.print(String.format("%s", this.desStudent));
+        System.out.print(String.format("%s", this.description));
     }
 }
 
-class School{
+class School {
     ArrayList<Person> persons = new ArrayList<Person>();
-    public void addPerson(Person person){
+
+    public void addPerson(Person person) {
         persons.add(person);
     }
-    public void ringBell(){
-        for (Person person : persons){
+
+    public void ringBell() {
+        for (Person person : persons) {
             person.whoAmI();
             System.out.println("");
         }
     }
-    public Person getPersonByName(String name){
-        for (Person person : persons){
-            if (person.name.equals(name)){
+
+    public Person getPersonByName(String name) {
+        for (Person person : persons) {
+            if (person.name.equals(name)) {
                 System.out.println("");
                 person.whoAmI();
                 System.out.println("");
@@ -118,15 +137,15 @@ class School{
 }
 
 public class POOSchool {
-    public static void main(String[] args){
-        Person pe1=new Person("Person-1");
-        Employee em1=new Employee("Employee-1");
-        Principal pr1=new Principal("Principal-1");
-        Teacher t1=new Teacher("Gabrielle Russier", "arts");
-        Volunteer v1=new Volunteer("Volunteer-1");
-        Kitchen k1=new Kitchen("Kitchen-1");
-        Cleaning c1=new Cleaning("Cleaning-1");
-        Student s1=new Student("Maddie Ziegler", "primary");
+    public static void main(String[] args) {
+        Person pe1 = new Person("Person-1");
+        Employee em1 = new Employee("Employee-1");
+        Principal pr1 = new Principal("Principal-1");
+        Teacher t1 = new Teacher("Gabrielle Russier", "arts");
+        Volunteer v1 = new Volunteer("Volunteer-1");
+        Kitchen k1 = new Kitchen("Kitchen-1");
+        Cleaning c1 = new Cleaning("Cleaning-1");
+        Student s1 = new Student("Maddie Ziegler", "primary");
 
         School sc1 = new School();
         sc1.addPerson(pe1);
@@ -141,11 +160,19 @@ public class POOSchool {
 
         sc1.ringBell(); // ringbell returns the description of each person of the school
 
-        Person person=sc1.getPersonByName("Gabrielle Russier"); // renames Person name = "Gabrielle Russier"
+        Person person = sc1.getPersonByName("Gabrielle Russier"); // renames Person name = "Gabrielle Russier"
+        // je renomme person (qui correspond à la base à "Gabrielle Russier")
         person.name = "Gabriel R.";
-        System.out.println(String.format("person.name, %s", person.name));
+        // je check qui est person
+        person.whoAmI();
+
+        // je renomme t1 (qui correspond aussi à la base à "Gabrielle Russier")
         t1.name = "G. R.";
-        System.out.println(String.format("person.name, %s ; t1.name %s", person.name, t1.name));
+        // je check qui sont person et t1, et je peux voir leur noms respectifs. (ils
+        // montrent le meme nom puisque person et t1 pointent vers la meme personne qui
+        // était à l'origine "Gabrielle Russier" et qui s'appelle maintenant "G. R.")
+        person.whoAmI();
+        t1.whoAmI();
 
     }
 }
