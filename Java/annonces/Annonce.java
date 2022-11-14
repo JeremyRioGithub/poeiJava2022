bed1a1cbf69fee0284c85087d0cf73bf29b921f9
Jeremy.Rio <rio.jeremy@outlook.fr>
2022-11-14 14:00:34 +0100

    working on imports, projet LeBonCoin v5.1: moved TAX calculation as double from main to Annonce

--- Java/annonces/Annonce.java
+++ Java/annonces/Annonce.java
@@ -9,11 +9,11 @@ public class Annonce {
     public Annonce(){}
     public Annonce(String titre, String prix, String descriptif) {
         this.titre=String.format("<< %s >>", titre);
-        this.prix=prix;
+        this.prix="***"+Double.toString(Float.parseFloat(prix+".0") * 1.2) +" (incl. 20% TAX)***";
         this.descriptif=String.format("[ %s ]", descriptif);
     }
     public String toString() {
-        return String.format("%s (%s)", this.titre, this.prix);
+        return String.format("%s %s", this.titre, this.prix);
     }
     public void afficher(){
         System.out.println(String.format("%s", this.descriptif));
@@ -22,7 +22,7 @@ public class Annonce {
         System.out.println("titre : ");
         this.titre=String.format("<< %s >>", sc1.nextLine());
         System.out.println("prix : ");
-        this.prix=sc1.nextLine();
+        this.prix="***"+Double.toString(Float.parseFloat(sc1.nextLine()+".0") * 1.2) +" (incl. 20% TAX)***";
         System.out.println("description : ");
         this.descriptif=String.format("[ %s ]", sc1.nextLine());
     }
