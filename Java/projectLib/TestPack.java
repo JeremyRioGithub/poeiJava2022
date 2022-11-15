4c2f0a51ba2facfdbf14580d797f3a478f72cc7d
Jeremy.Rio <rio.jeremy@outlook.fr>
2022-11-15 11:44:44 +0100

    projectLib v1.0: Run, says 'je suis la classe A', by using method classeA from importing library myLib in folder projectLib

---
+++ Java/projectLib/TestPack.java
@@ -0,0 +1,18 @@
+package projectLib;
+
+import projectLib.myLib.*;
+
+// Le Collectioneur de Papillon
+class  TestPack {
+ // Jeremy, from Xavier
+
+    public static void main(String[] args)
+    {
+        ClasseA ka = new ClasseA();
+
+        System.out.println(  ka  );
+        // je suis la classe A 
+    }        
+}
+
+
---
+++ Java/projectLib/myLib/ClasseA.java
@@ -0,0 +1,12 @@
+package projectLib.myLib;
+
+public class ClasseA {
+
+    public ClasseA() {
+    }
+
+    public String toString() {
+        return "je suis la classe A";
+    }
+
+}
