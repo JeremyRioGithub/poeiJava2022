74e6fd23709efb92768d495d9bb3f5df4de10095
Jeremy.Rio <rio.jeremy@outlook.fr>
2022-11-15 14:16:18 +0100

    test return expected v1.0

---
+++ Java/LesTestOne.java
@@ -0,0 +1,58 @@
+// Bourget
+
+class Atester 
+{
+    static public Integer calcul1( int var )
+    {
+        return 5 * var;
+    }
+
+
+    static public String presentation( String n )
+    {  
+        if (n.equals("")){
+            return n;
+        }
+        System.out.println(n.toUpperCase().charAt(0) +n.toLowerCase().substring(1));
+        return n.charAt(0)+n.substring(1);
+    }
+
+
+
+} 
+
+
+public class LesTestOne {
+    public static void main( String[] args )
+    {
+        if ( Atester.calcul1( 3 ) == 15 )
+            System.out.println( "OK" );
+
+        if ( Atester.calcul1( 0 ) == 0 )
+            System.out.println( "OK" );
+
+        if ( Atester.calcul1( 1 ) == 5 )
+
+        /* ******************************* */
+
+        if ( Atester.presentation( "toto" ).equals( "Toto") )
+            System.out.println( "OK" );
+        else
+            System.out.println( "KO" );
+ 
+        if ( Atester.presentation( "TOTO" ).equals( "Toto") )
+            System.out.println( "OK" );
+        else
+            System.out.println( "KO" );
+ 
+        if ( Atester.presentation( "" ).equals( "") )
+            System.out.println( "OK" );
+        else
+            System.out.println( "KO" );
+ 
+        if ( Atester.presentation( "1234567890" ).equals( "1234567890") )
+            System.out.println( "OK" );
+        else
+            System.out.println( "KO" );
+    }    
+}
