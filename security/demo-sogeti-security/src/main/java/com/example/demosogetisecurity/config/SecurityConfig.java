a79adc6c55084738113ded8d58fbfd9f24eff59c
Jeremy.Rio <rio.jeremy@outlook.fr>
2023-01-03 09:43:53 +0100

    wip inmemoryuserdetails

--- security/demo-sogeti-security/src/main/java/com/example/demosogetisecurity/config/SecurityConfig.java
+++ security/demo-sogeti-security/src/main/java/com/example/demosogetisecurity/config/SecurityConfig.java
@@ -3,6 +3,11 @@ package com.example.demosogetisecurity.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 // avant de charger le projet, on charge les configurations présentes la ou se trouve l'annotation @Configuration
@@ -24,5 +29,19 @@ public class SecurityConfig {
                 .and().formLogin()
                 .and().httpBasic();
       return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("12345")
+                .authorities("admin").build();
+        return new InMemoryUserDetailsManager();
     }
 }
