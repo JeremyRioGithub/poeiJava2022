4d182b857594da6379e1259c5cfb8a515d2b0021
Jeremy.Rio <rio.jeremy@outlook.fr>
2023-01-03 16:31:01 +0100

    updated controllers

--- security/demo-sogeti-security/src/main/java/com/example/demosogetisecurity/config/SecurityConfig.java
+++ security/demo-sogeti-security/src/main/java/com/example/demosogetisecurity/config/SecurityConfig.java
@@ -5,43 +5,57 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 // avant de charger le projet, on charge les configurations présentes la ou se trouve l'annotation @Configuration
 @Configuration
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-      /*http.authorizeHttpRequests()
+      http.csrf().disable()
+              .authorizeHttpRequests()
               .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
-              .requestMatchers("/myNotices", "/myContact").permitAll()
+              .requestMatchers("/myNotices", "/myContact", "/register").permitAll()
               .and().formLogin()
-              .and().httpBasic();*/
+              .and().httpBasic();
 //empecher tout acces
         /*http.authorizeHttpRequests().anyRequest().denyAll()
                 .and().formLogin()
                 .and().httpBasic();*/
 //autoriser tout acces
-        http.authorizeHttpRequests().anyRequest().permitAll()
+        /*http.authorizeHttpRequests().anyRequest().permitAll()
                 .and().formLogin()
-                .and().httpBasic();
+                .and().httpBasic();*/
       return http.build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+        /*return NoOpPasswordEncoder.getInstance();*/
+        return new BCryptPasswordEncoder();
     }
 
-    @Bean
+    // determine user and pass and add user authorities here
+    /*@Bean
     public InMemoryUserDetailsManager userDetailsManager(){
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin")
+        UserDetails admin = User.withUsername("user")
                 .password("12345")
-                .authorities("admin").build();
+                .authorities("read").build();
         return new InMemoryUserDetailsManager();
-    }
+    }*/
+
+    // load user+pass list with its authorities from MYSQL table
+    /*@Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }*/
+
 }
--- security/demo-sogeti-security/src/main/java/com/example/demosogetisecurity/controller/LoansController.java
+++ security/demo-sogeti-security/src/main/java/com/example/demosogetisecurity/controller/LoansController.java
@@ -1,10 +1,28 @@
 package com.example.demosogetisecurity.controller;
 
+import com.example.demosogetisecurity.model.Loans;
+import com.example.demosogetisecurity.repository.LoanRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class LoansController {
+    @Autowired
+    private LoanRepository loanRepository;
+
+    @GetMapping("/myLoans")
+    public List<Loans> getLoanDetails(@RequestParam int id) {
+        List<Loans> loans = loanRepository.findByCustomerIdOrderByStartDtDesc(id);
+        if (loans != null ) {
+            return loans;
+        }else {
+            return null;
+        }
+    }
     @GetMapping("/myLoans")
     public String getLoansDetails(){
         return "Bienvenue dans le detail du Loans.";
